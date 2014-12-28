069ff355e2395c1a3eb48e48bcbfe5b44a9a3efa
dmitry.bilyk <dmitry.b@scopicsoftware.com>
2014-12-28 22:06:08 +0200

    remove redundant code from interview

--- src/main/java/com/learn/gwttoknow/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by dik81 on 12/8/14.
- */
-public class Main {
-  public static void main(String[] args) {
-
-  }
-}
--- src/main/java/com/learn/gwttoknow/src/main/java/toknow/model/builder/CompanyBOBuilder.java
+++
@@ -1,7 +0,0 @@
-package toknow.model.builder;
-
-/**
- * Created by dik81 on 12/8/14.
- */
-public class CompanyBOBuilder {
-}
