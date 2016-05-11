ef4a2ebf31b0277c011ec034fddac23756b883d9
dmitrybilyk <03564503g>
2016-05-11 22:27:00 +0300

    refactoring.guru

---
+++ src/main/java/com/learn/effectivejava/refactoring/refactoring_guru/utyazelitely_izmeneniy/vstraivaniye_classa.java
@@ -0,0 +1,76 @@
+package com.learn.effectivejava.refactoring.refactoring_guru.utyazelitely_izmeneniy;
+
+/**
+ * Created by Dimon on 11.05.2016.
+ */
+public class vstraivaniye_classa {
+//    class Person {
+//        private String name;
+//        private TelephoneNumber officeTelephone = new TelephoneNumber();
+//
+//        public String getName() {
+//            return name;
+//        }
+//        public String getTelephoneNumber() {
+//            return officeTelephone.getTelephoneNumber();
+//        }
+//        public TelephoneNumber getOfficeTelephone() {
+//            return officeTelephone;
+//        }
+//    }
+//
+//    class TelephoneNumber {
+//        private String number;
+//        private String areaCode;
+//
+//        public String getAreaCode() {
+//            return areaCode;
+//        }
+//        public void setAreaCode(String arg) {
+//            areaCode = arg;
+//        }
+//        public String getNumber() {
+//            return number;
+//        }
+//        public void setNumber(String arg) {
+//            number = arg;
+//        }
+//        public String getTelephoneNumber() {
+//            return ("(" + areaCode + ") " + number);
+//        }
+//    }
+//
+//    // Somewhere in client code
+//    Person martin = new Person();
+//    martin.getOfficeTelephone().setAreaCode("781");
+//
+//    class Person {
+//        private String name;
+//        private String number;
+//        private String areaCode;
+//
+//        public String getName() {
+//            return name;
+//        }
+//        public String getTelephoneNumber() {
+//            return ("(" + areaCode + ") " + number);
+//        }
+//        public String getAreaCode() {
+//            return areaCode;
+//        }
+//        public void setAreaCode(String arg) {
+//            areaCode = arg;
+//        }
+//        public String getNumber() {
+//            return number;
+//        }
+//        public void setNumber(String arg) {
+//            number = arg;
+//        }
+//    }
+//
+//
+//    // Somewhere in client code
+//    Person martin = new Person();
+//    martin.setAreaCode("781");
+}
---
+++ src/main/java/com/learn/effectivejava/refactoring/refactoring_guru/zamusorivateli/komentarii/vvedenie_proverki_utverzdeniya.java
@@ -0,0 +1,24 @@
+package com.learn.effectivejava.refactoring.refactoring_guru.zamusorivateli.komentarii;
+
+/**
+ * Created by Dimon on 11.05.2016.
+ */
+public class vvedenie_proverki_utverzdeniya {
+//    double getExpenseLimit() {
+//        // should have either expense limit or a primary project
+//        return (expenseLimit != NULL_EXPENSE) ?
+//                expenseLimit:
+//                primaryProject.getMemberExpenseLimit();
+//    }
+//
+//    TO
+//
+//    double getExpenseLimit() {
+//        Assert.isTrue(expenseLimit != NULL_EXPENSE || primaryProject != null);
+//
+//        return (expenseLimit != NULL_EXPENSE) ?
+//                expenseLimit:
+//                primaryProject.getMemberExpenseLimit();
+//    }
+
+}
