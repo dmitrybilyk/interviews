f954056cd8cc4a07f5169f12848705fee6f82774
dmitry <dmitry.bilyk@gmail.com>
2017-01-18 22:26:34 +0200

    iterator pattern

---
+++ src/main/java/com/learn/patterns/behavioral/trainings/iterator/Player.java
@@ -0,0 +1,25 @@
+package com.learn.patterns.behavioral.trainings.iterator;
+
+/**
+ * Created by dmitry on 18.01.17.
+ */
+public class Player {
+    private String name;
+    private Position position;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
---
+++ src/main/java/com/learn/patterns/behavioral/trainings/iterator/Position.java
@@ -0,0 +1,8 @@
+package com.learn.patterns.behavioral.trainings.iterator;
+
+/**
+ * Created by dmitry on 18.01.17.
+ */
+public enum Position {
+    GOALKEEPER, DEFENDER, MIDDFIELDER, STRIKER
+}
