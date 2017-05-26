463a636081809053098bb1d82a09eaa00090e87c
dmitrybilyk <dmitry.bilyk@gmail.com>
2017-05-26 09:02:47 +0300

    angular

--- web/src/main/webapp/angular/angular-hello/src/app/app.module.ts
+++ web/src/main/webapp/angular/angular-hello/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
