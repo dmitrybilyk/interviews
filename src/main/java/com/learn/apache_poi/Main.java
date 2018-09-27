232226404f12f9c3034ea8ae93bde4afef03a7de
bilyk <dmitry.b@scopicsoftware.com>
2018-09-27 17:06:05 +0300

    apache poi

---
+++ src/main/java/com/learn/apache_poi/Main.java
@@ -0,0 +1,77 @@
+package com.learn.apache_poi;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by dmitry on 27.09.18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("Datatypes in Java");
+        Object[][] datatypes = {
+                {"Datatype", "Type", "Size(in bytes)"},
+                {"int", "Primitive", 2},
+                {"float", "Primitive", 4},
+                {"double", "Primitive", 8},
+                {"char", "Primitive", 1},
+                {"String", "Non-Primitive", "No fixed size"}
+        };
+
+        int rowNum = 0;
+        System.out.println("Creating excel");
+
+        for (Object[] datatype : datatypes) {
+            Row row = sheet.createRow(rowNum++);
+            int colNum = 0;
+            for (Object field : datatype) {
+                Cell cell = row.createCell(colNum++);
+                XSSFCellStyle cellStyle = workbook.createCellStyle();
+                XSSFFont font = workbook.createFont();
+                font.setItalic(true);
+//                XSSFColor color = new XSSFColor();
+//                color.setRgb(javax.xml.bind.DatatypeConverter.parseHexBinary("#3f3f3f"));
+//                font.setColor(color);
+                cellStyle.setFont(font);
+                font.setColor(IndexedColors.GREY_50_PERCENT.getIndex());
+//                cellStyle.setFillPattern(IndexedColors.GREY_80_PERCENT.getIndex());
+                cell.setCellStyle(cellStyle);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream("test_poi.xlsx");
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                workbook.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Done");
+    }
+}
