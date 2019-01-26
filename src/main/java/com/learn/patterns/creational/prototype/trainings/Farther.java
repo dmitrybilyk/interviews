56343ac332845c6c7232a99d3fb1bee6c3392589
bilyk <dmitry.b@scopicsoftware.com>
2019-01-26 16:32:23 +0200

    initial old

---
+++ src/main/java/com/learn/patterns/creational/prototype/trainings/Farther.java
@@ -0,0 +1,21 @@
+package com.learn.patterns.creational.prototype.trainings;
+
+/**
+ * Created by dmitry on 26.01.19.
+ */
+public class Farther implements Cloneable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected Farther clone() throws CloneNotSupportedException {
+        return (Farther)super.clone();
+    }
+}
