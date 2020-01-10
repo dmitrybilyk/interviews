16cfea0918e9a2a147dec38af53c7b28d5d3f2e9
bilyk <dmitry.b@scopicsoftware.com>
2020-01-10 17:51:23 +0200

    interviews

---
+++ src/main/java/com/learn/interview_questions/Main.java
@@ -0,0 +1,12 @@
+package com.learn.interview_questions;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+	interface someInterface {
+		int d = 0;
+		void pring();
+		void print2();
+	}
+}
