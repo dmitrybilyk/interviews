b57673e01396c5ecc9b19d4af340f8a76736900d
dmitry <dmitry.b@scopicsoftware.com>
2020-05-12 19:27:03 +0300

    blocking queue

---
+++ src/main/java/com/learn/core/multithreading/mkyong/BlockingQueueExample.java
@@ -0,0 +1,56 @@
+package com.learn.core.multithreading.mkyong;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueExample
+{
+    private BlockingQueue<String> drop;
+
+    private final String    DONE     = "done";
+    private final String[]  messages = {
+                            "Мама пошла готовить обед",
+                            "Мама позвала к столу",
+                            "Дети кушают молочную кашу",
+                            "А что ест папа?"};	
+    public BlockingQueueExample() 
+    {
+        drop = new ArrayBlockingQueue<String>(1, false);
+        (new Thread(new Producer())).start();
+        (new Thread(new Consumer())).start();
+    }
+
+    class Producer implements Runnable 
+    {
+        public void run() {
+            try {
+                int cnt = 0;
+                for (int i=0; i<messages.length; i++) {
+                    drop.put(messages[i]);
+                    if (++cnt < 3)
+                        Thread.sleep(2000);
+                }
+                drop.put(DONE);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    class Consumer implements Runnable
+    {
+        public void run() {
+            try {
+                String msg = null;
+                while (!((msg = drop.take()).equals(DONE)))
+                    System.out.println(msg);
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new BlockingQueueExample();
+    }
+}
