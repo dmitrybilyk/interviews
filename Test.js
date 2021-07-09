dc6a1f3ebc7b7fd5ebaeaa14adae677374a5cbb3
dmitry.bilyk <dmitry.b@scopicsoftware.com>
2021-07-09 15:54:03 +0300

    rights from keycloak

--- Test.js
+++ Test.js
@@ -25,11 +25,11 @@ const parsePermissions = (data, state) => {
 
     const getAllAtomicRolesClosure = (roles) => {
         const getRolesWithCompositesAcc = (role, acc) => {
-            if (role.composites) {
-                return role.composites.flatMap((compositeRole) => getRolesWithCompositesAcc(compositeRole, acc.concat(role.name)))
-            } else {
+            // if (role.composites) {
+            //     return role.composites.flatMap((compositeRole) => getRolesWithCompositesAcc(compositeRole, acc.concat(role.name)))
+            // } else {
                 return acc.concat(role.name)
-            }
+            // }
         }
         return roles.flatMap(
             (role) => getRolesWithCompositesAcc(role, [])
@@ -47,14 +47,12 @@ const parsePermissions = (data, state) => {
         'canViewConversationsScreen',
         ramda_1.allPass([
             ramda_1.includes(VIEW_CONVERSATIONS_SCREEN),
-            ramda_1.anyPass(
-                ramda_1.map(ramda_1.includes, [
-                    'INTERACTIONS_FULL_VIEW',
-                    'INTERACTIONS_AGENT_VIEW',
-                    'INTERACTIONS_GROUP_VIEW',
-                    'INTERACTION_REVIEWS_VIEW',
-                ])
-            ),
+            ramda_1.anyPass([
+                ramda_1.includes('INTERACTIONS_FULL_VIEW'),
+                ramda_1.includes('INTERACTIONS_AGENT_VIEW'),
+                ramda_1.includes('INTERACTIONS_GROUP_VIEW'),
+                ramda_1.includes('INTERACTION_REVIEWS_VIEW'),
+            ]),
         ])(allAtomicRoles)
     )(simplePermissions);
 
@@ -64,7 +62,9 @@ const parsePermissions = (data, state) => {
 exports.parsePermissions = parsePermissions;
 // console.log(exports.parsePermissions({ "username": "ccmanager", "userId": "2fae446b-4ed9-4775-bcca-9a8e287c07a0", "firstName": "CcManager", "lastName": "CcManager", "sourceId": "SCORE_CARD", "email": "jane.doe@company.domain", "enabled": true, "roles": [{ "name": "view-profile" }, { "name": "VIEW_CONVERSATIONS_SCREEN" }, { "name": "offline_access" }, { "name": "manage-account", "composites": [{ "name": "manage-account-links" }] }, { "name": "keycloak-event-provider" }, { "name": "DEFAULT_CC_MANAGER", "composites": [{ "name": "DATA_EXPORT_ALL_RESULTS" }, { "name": "REPORTING_ALL" }, { "name": "MANAGE_QUESTIONNAIRES" }, { "name": "REPORTING_SELF" }, { "name": "INTERACTION_ANONYMIZE" }, { "name": "INTERACTION_TAGS_VIEW" }, { "name": "REALLOCATE_EVALUATIONS" }, { "name": "EDIT_GLOBAL_TEMPLATES" }, { "name": "CREATE_EVALS" }, { "name": "EDIT_ADMIN_SETTING" }, { "name": "REPORTING_ASSIGNED_TEAMS" }, { "name": "INTERACTION_REVIEWS_VIEW" }, { "name": "MANAGE_DATA_LIFECYCLE" }, { "name": "EDIT_INTERACTION_TYPES" }, { "name": "REPLACE_CALLS" }, { "name": "INTERACTION_TAGS_MANAGE" }, { "name": "MANAGE_SPEECH_TAG" }, { "name": "ADD_AUDIT_REASON" }, { "name": "INTERACTIONS_FULL_VIEW" }, { "name": "EVAL_REMOVE_ALL" }, { "name": "DATA_TASK_ALL" }, { "name": "MANAGE_SHARED_DASHBOARD" }, { "name": "INTERACTION_TAGS_ADD" }, { "name": "EVAL_AGENTS" }, { "name": "SCORING_TIERS_MANAGE" }, { "name": "MANAGE_COMPLIANCE" }, { "name": "ADD_CALL" }, { "name": "INTERACTIONS_GROUP_VIEW" }, { "name": "EDIT_AUDIT_REASON" }, { "name": "INTERACTIONS_EXPORT_MEDIA" }, { "name": "INTERACTION_TAGS_EDIT" }, { "name": "VIEW_MY_EVALS" }, { "name": "SCORING_TIERS_VIEW" }, { "name": "VIEW_TEAM_EVALS" }, { "name": "PLAN_ALL_EVALS" }, { "name": "VIEW_ALL_EVALS" }, { "name": "PLAN_GROUP_EVALS" }, { "name": "VIEW_AUDIT" }, { "name": "INTERACTIONS_EXPORT_JSON" }, { "name": "INTERACTIONS_AGENT_VIEW" }, { "name": "VIEW_ZPA" }] }], "groups": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "mainGroupId": "4f1c68c2-a645-4dc0-b63b-537b7432daea", "scorecardId": "2", "usersTeams": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "created": 1619773494885, "locale": "en-US", "timezone": "UTC" }, null));
 
-console.log(exports.parsePermissions({ "username": "ccmanager", "userId": "2fae446b-4ed9-4775-bcca-9a8e287c07a0", "firstName": "CcManager", "lastName": "CcManager", "sourceId": "SCORE_CARD", "email": "jane.doe@company.domain", "enabled": true, "roles": [{ "name": "view-profile" }, { "name": "VIEW_CONVERSATIONS_SCREEN" }, { "name": "offline_access" }, { "name": "manage-account", "composites": [{ "name": "manage-account-links" }] }, { "name": "keycloak-event-provider" }, { "name": "INTERACTIONS_FULL_VIEW" }, { "name": "INTERACTIONS_AGENT_VIEW" }, { "name": "INTERACTIONS_GROUP_VIEW" }, { "name": "INTERACTION_REVIEWS_VIEW" }], "groups": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "mainGroupId": "4f1c68c2-a645-4dc0-b63b-537b7432daea", "scorecardId": "2", "usersTeams": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "created": 1619773494885, "locale": "en-US", "timezone": "UTC" }, null));
+// console.log(exports.parsePermissions({ "username": "ccmanager", "userId": "2fae446b-4ed9-4775-bcca-9a8e287c07a0", "firstName": "CcManager", "lastName": "CcManager", "sourceId": "SCORE_CARD", "email": "jane.doe@company.domain", "enabled": true, "roles": [{ "name": "view-profile" }, { "name": "VIEW_CONVERSATIONS_SCREEN" }, { "name": "offline_access" }, { "name": "manage-account", "composites": [{ "name": "manage-account-links" }] }, { "name": "keycloak-event-provider" }, { "name": "INTERACTIONS_FULL_VIEW" }, { "name": "INTERACTIONS_AGENT_VIEW" }, { "name": "INTERACTIONS_GROUP_VIEW" }, { "name": "INTERACTION_REVIEWS_VIEW" }], "groups": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "mainGroupId": "4f1c68c2-a645-4dc0-b63b-537b7432daea", "scorecardId": "2", "usersTeams": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "created": 1619773494885, "locale": "en-US", "timezone": "UTC" }, null));
+
+console.log(exports.parsePermissions({ "username": "ccmanager", "userId": "2fae446b-4ed9-4775-bcca-9a8e287c07a0", "firstName": "CcManager", "lastName": "CcManager", "sourceId": "SCORE_CARD", "email": "jane.doe@company.domain", "enabled": true, "roles": [{"name":"view-profile"},{"name":"INTERACTIONS_AGENT_VIEW","composites":[]},{"name":"VIEW_TEAM_EVALS","composites":[]},{"name":"INTERACTION_TAGS_ADD","composites":[]},{"name":"manage-account","composites":[{"name":"manage-account-links"}]},{"name":"PLAN_ALL_EVALS","composites":[]},{"name":"SCORING_TIERS_VIEW","composites":[]},{"name":"MANAGE_COMPLIANCE","composites":[]},{"name":"EVAL_AGENTS","composites":[]},{"name":"VIEW_ZPA","composites":[]},{"name":"EDIT_AUDIT_REASON","composites":[]},{"name":"INTERACTIONS_EXPORT_JSON","composites":[]},{"name":"PLAN_GROUP_EVALS","composites":[]},{"name":"INTERACTION_TAGS_EDIT","composites":[]},{"name":"ADD_CALL","composites":[]},{"name":"VIEW_AUDIT","composites":[]},{"name":"INTERACTIONS_GROUP_VIEW","composites":[]},{"name":"INTERACTIONS_FULL_VIEW","composites":[]},{"name":"DATA_TASK_ALL","composites":[]},{"name":"EDIT_GLOBAL_TEMPLATES","composites":[]},{"name":"EVAL_REMOVE_ALL","composites":[]},{"name":"CREATE_EVALS","composites":[]},{"name":"SCORING_TIERS_MANAGE","composites":[]},{"name":"VIEW_CONVERSATIONS_SCREEN","composites":[]},{"name":"VIEW_MY_EVALS","composites":[]},{"name":"INTERACTIONS_EXPORT_MEDIA","composites":[]},{"name":"INTERACTION_ANONYMIZE","composites":[]},{"name":"EDIT_ADMIN_SETTING","composites":[]},{"name":"VIEW_ALL_EVALS","composites":[]},{"name":"EDIT_INTERACTION_TYPES","composites":[]},{"name":"MANAGE_DATA_LIFECYCLE","composites":[]},{"name":"REALLOCATE_EVALUATIONS","composites":[]},{"name":"INTERACTION_TAGS_VIEW","composites":[]},{"name":"DEFAULT_CC_MANAGER","composites":[]},{"name":"REPLACE_CALLS","composites":[]},{"name":"INTERACTION_REVIEWS_VIEW","composites":[]},{"name":"REPORTING_ASSIGNED_TEAMS","composites":[]},{"name":"MANAGE_QUESTIONNAIRES","composites":[]},{"name":"DATA_EXPORT_ALL_RESULTS","composites":[]},{"name":"MANAGE_SHARED_DASHBOARD","composites":[]},{"name":"INTERACTION_TAGS_MANAGE","composites":[]},{"name":"ADD_AUDIT_REASON","composites":[]},{"name":"REPORTING_SELF","composites":[]},{"name":"REPORTING_ALL","composites":[]}], "groups": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "mainGroupId": "4f1c68c2-a645-4dc0-b63b-537b7432daea", "scorecardId": "2", "usersTeams": ["4f1c68c2-a645-4dc0-b63b-537b7432daea"], "created": 1619773494885, "locale": "en-US", "timezone": "UTC" }, null));
 
 
 
