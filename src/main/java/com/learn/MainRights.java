8ec656ea89dfe6a30b080830a33a64e6042244cd
Dmytro <dmitry.b@scopicsoftware.com>
2022-03-29 18:46:28 +0300

    interview

--- src/main/java/com/learn/MainRights.java
+++ src/main/java/com/learn/MainRights.java
@@ -49,6 +49,14 @@ public class MainRights {
 
 @Data
 class Role {
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   private String name;
   private List<Role> composites;
 }
